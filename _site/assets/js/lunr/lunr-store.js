ce9897668fd5cdfb59ce9814b2aea1c1edac2aca
doublejkim <jaejin.km@gmail.com>
2021-12-09 12:15:24 +0900

    acid 포스팅

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,16 @@ var store = [{
         "tags": [],
         "url": "/Start-History/",
         "teaser": null
+      },{
+        "title": "miniDLNA 구동 후 접속 기기에서 파일이 보이지 않을경우",
+        "excerpt":"miniDLNA ? 손쉽게 미디어 서버를 구축할 수 있는 miniDLNA 라는 서버가 있으며, DLNA/UPnP 를 지원하는 스마트 TV 나 그에 대응되는 기기가 있을 경우 동일 네트워크 상에서 손쉽게 영상 플레이 등이 가능합니다. 현재 이문서는 miniDLNA 설치를 다루지는 않으며, 파일이 보이지 않을경우의 트러블 슈팅만 언급하겠습니다. 파일이 보이지 않을 경우에는 status 확인 miniDLNA...","categories": ["linux"],
+        "tags": [],
+        "url": "/linux/Minidlna-troubleshooting1/",
+        "teaser": null
+      },{
+        "title": "트랜잭션에서의 ACID",
+        "excerpt":"트랜잭션 ACID 트랜잭션 (Transaction) 트랜잭션이란 질의(query)를 하나의 묶음으로 처리하도록하는 작업수행의 논리적 단위 입니다. 트랜잭션은 ACID(원자성, 일관성, 독립성, 지속성)라는 특성을 만족해야합니다. 원자성 (Atomicity) 트랜잭션의 작업이 부분적으로 실행되거나 중단되지 않음을 보장하는 특성입니다. 흔히 All or Nothing 이라고 부르는 개념으로 작업단위를 일부분만 실행하지 않고 실행이 되지 않는 상황이면 아무것도 실행되지 않아야하며 실행이 되었다면...","categories": ["db"],
+        "tags": [],
+        "url": "/db/transaction_acid1/",
+        "teaser": null
       }]
